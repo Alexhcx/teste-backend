cd6a65d7a60f90425287c13fcc4144d104236923
Alexandre Nazareth <alexandrenazareth@hotmail.com>
2025-09-29 14:15:15 -0300

    fix: correct database variable name in production configuration

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -27,7 +27,7 @@ const test = {
 const production = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_DATABASE,
   host: process.env.DB_HOSTNAME,
   port: process.env.DB_PORT,
   dialect: 'mysql',
