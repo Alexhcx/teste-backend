27477b9ea2e5676fd1608f7162c42d3a70772997
Alexandre Nazareth <alexandrenazareth@hotmail.com>
2025-09-29 13:28:33 -0300

    fix: use consistent port variable for server listening

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -25,6 +26,6 @@ app.get('/health', (req, res) => {
 
 app.use('/api', productRoutes);
 
-app.listen(process.env.PORT, 'localhost', () => {
-    console.log(`API rodando na porta ${process.env.PORT}`);
+app.listen(port, 'localhost', () => {
+    console.log(`API rodando na porta ${port}`);
 });
