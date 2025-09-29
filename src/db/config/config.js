32bd8443c592141a17d1632743b7e8cb700f8adb
Alexandre Nazareth <alexandrenazareth@hotmail.com>
2025-09-29 13:41:18 -0300

    fix: update database configuration to use environment variables for host and port

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -4,8 +4,8 @@ const development = {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  host: '127.0.0.1',
-  port: 3306,
+  host: process.env.DB_HOSTNAME,
+  port: process.env.DB_PORT,
   dialect: 'mysql',
   dialectOptions: {
     bigNumberStrings: true,
@@ -16,8 +16,8 @@ const test = {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  host: '127.0.0.1',
-  port: 3306,
+  host: process.env.DB_HOSTNAME,
+  port: process.env.DB_PORT,
   dialect: 'mysql',
   dialectOptions: {
     bigNumberStrings: true,
@@ -25,11 +25,11 @@ const test = {
 };
 
 const production = {
-  username: process.env.PROD_DB_USERNAME,
-  password: process.env.PROD_DB_PASSWORD,
-  database: process.env.PROD_DB_NAME,
-  host: process.env.PROD_DB_HOSTNAME,
-  port: process.env.PROD_DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOSTNAME,
+  port: process.env.DB_PORT,
   dialect: 'mysql',
   dialectOptions: {
     bigNumberStrings: true,
